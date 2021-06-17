6e2f9bf0244f1ec7bedf60a4b603894c64e1a81d
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2021-06-17 15:51:39 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/canvas_to_image/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.canvas_to_image
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,292 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(CanvasToImageApp());
+}
+
+class CanvasToImageApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Canvas to Image',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CanvasToImagePage(),
+    );
+  }
+}
+
+class CanvasToImagePage extends StatefulWidget {
+  @override
+  _CanvasToImagePageState createState() => _CanvasToImagePageState();
+}
+
+enum _BufferBuilder {
+  off,
+  picture,
+  imageFromPictureRecorder,
+  imageFromRepaintBoundary,
+  imageProviderFromPictureRecorder,
+  imageProviderFromRepaintBoundary
+}
+
+class _CanvasToImagePageState extends State<CanvasToImagePage> {
+  final Size _size = Size(500, 300);
+  final GlobalKey _repaintKey = GlobalKey();
+
+  _BufferBuilder _bufferBuilder = _BufferBuilder.off;
+
+  bool _bufferLoading = false;
+
+  // buffers
+  ui.Picture? _picture;
+  ui.Image? _image;
+  ImageProvider? _imageProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(children: [
+      _buildMenu(),
+      Expanded(
+          child: Center(
+              child: Container(
+                  child: _buildCanvas(),
+                  decoration: BoxDecoration(border: Border.all()),
+                  width: _size.width,
+                  height: _size.height)))
+    ], crossAxisAlignment: CrossAxisAlignment.stretch));
+  }
+
+  Widget _buildMenu() {
+    return Padding(
+        child: Column(
+          children: [
+            Padding(
+                child: Text('Buffer:'), padding: EdgeInsets.only(bottom: 16)),
+            _buildMenuButton('Off', _BufferBuilder.off),
+            _buildMenuButton(
+                'PictureRecorder > Picture', _BufferBuilder.picture),
+            _buildMenuButton('PictureRecorder > Image',
+                _BufferBuilder.imageFromPictureRecorder),
+            _buildMenuButton('RepaintBoundary > Image',
+                _BufferBuilder.imageFromRepaintBoundary),
+            _buildMenuButton('PictureRecorder > ImageProvider',
+                _BufferBuilder.imageProviderFromPictureRecorder),
+            _buildMenuButton('RepaintBoundary > ImagePovider',
+                _BufferBuilder.imageProviderFromRepaintBoundary)
+          ],
+        ),
+        padding: EdgeInsets.all(16));
+  }
+
+  Widget _buildMenuButton(String text, _BufferBuilder bufferBuilder) {
+    TextButton textButton = TextButton(
+        onPressed: () {
+          if (!_bufferLoading) {
+            setState(() {
+              _bufferBuilder = bufferBuilder;
+              _picture = null;
+              _image = null;
+              _imageProvider = null;
+            });
+          }
+        },
+        child: Text(text));
+    Color? color = Colors.transparent;
+    if (_bufferBuilder == bufferBuilder) {
+      color = Colors.blue;
+    }
+    return Container(
+        child: textButton,
+        decoration: BoxDecoration(border: Border.all(color: color)),
+        padding: EdgeInsets.fromLTRB(8, 0, 8, 0));
+  }
+
+  Widget _buildCanvas() {
+    if (_bufferBuilder == _BufferBuilder.off) {
+      return CustomPaint(painter: _Painter(), child: Container());
+    } else if (_bufferBuilder == _BufferBuilder.picture) {
+      if (_picture != null) {
+        return CustomPaint(
+            painter: _BufferPainter(picture: _picture), child: Container());
+      }
+      _loadNewBuffer();
+      return Text('Loading...');
+    } else if (_bufferBuilder == _BufferBuilder.imageFromPictureRecorder) {
+      if (_image != null) {
+        return CustomPaint(
+            painter: _BufferPainter(image: _image), child: Container());
+      }
+      _loadNewBuffer();
+      return Text('Loading...');
+    } else if (_bufferBuilder == _BufferBuilder.imageFromRepaintBoundary) {
+      if (_image != null) {
+        return CustomPaint(
+            painter: _BufferPainter(image: _image), child: Container());
+      }
+      _loadNewBuffer();
+      return RepaintBoundary(
+          key: _repaintKey,
+          child: CustomPaint(painter: _Painter(), child: Container()));
+    } else if (_bufferBuilder ==
+        _BufferBuilder.imageProviderFromPictureRecorder) {
+      if (_imageProvider != null) {
+        return Image(
+            image: _imageProvider!,
+            isAntiAlias: true,
+            filterQuality: FilterQuality.high);
+      }
+      _loadNewBuffer();
+      return Text('Loading...');
+    } else if (_bufferBuilder ==
+        _BufferBuilder.imageProviderFromRepaintBoundary) {
+      if (_imageProvider != null) {
+        return Image(
+            image: _imageProvider!,
+            isAntiAlias: true,
+            filterQuality: FilterQuality.high);
+      }
+      _loadNewBuffer();
+      return RepaintBoundary(
+          key: _repaintKey,
+          child: CustomPaint(painter: _Painter(), child: Container()));
+    }
+    throw UnimplementedError(_bufferBuilder.toString());
+  }
+
+  _loadNewBuffer() {
+    if (!_bufferLoading) {
+      // running out of build
+      Future.delayed(Duration.zero, () {
+        setState(() {
+          _bufferLoading = true;
+        });
+        if (_bufferBuilder == _BufferBuilder.picture) {
+          _loadPicture().then((value) {
+            setState(() {
+              _picture = value;
+              _bufferLoading = false;
+            });
+          });
+        } else if (_bufferBuilder == _BufferBuilder.imageFromPictureRecorder) {
+          _loadImageFromPictureRecorder().then((value) {
+            setState(() {
+              _image = value;
+              _bufferLoading = false;
+            });
+          });
+        } else if (_bufferBuilder == _BufferBuilder.imageFromRepaintBoundary) {
+          _loadImageFromRepaintBoundary().then((value) {
+            setState(() {
+              _image = value;
+              _bufferLoading = false;
+            });
+          });
+        } else if (_bufferBuilder ==
+            _BufferBuilder.imageProviderFromPictureRecorder) {
+          _loadImageProviderFromPictureRecorder().then((value) {
+            setState(() {
+              _imageProvider = value;
+              _bufferLoading = false;
+            });
+          });
+        } else if (_bufferBuilder ==
+            _BufferBuilder.imageProviderFromRepaintBoundary) {
+          _loadImageProviderFromRepaintBoundary().then((value) {
+            setState(() {
+              _imageProvider = value;
+              _bufferLoading = false;
+            });
+          });
+        }
+      });
+    }
+  }
+
+  Future<ui.Picture> _loadPicture() async {
+    ui.PictureRecorder recorder = ui.PictureRecorder();
+    Canvas canvas = Canvas(recorder,
+        Rect.fromPoints(Offset.zero, Offset(_size.width, _size.height)));
+
+    _Painter painter = _Painter();
+    painter.paint(canvas, _size);
+
+    ui.Picture picture = recorder.endRecording();
+    return picture;
+  }
+
+  Future<ui.Image> _loadImageFromPictureRecorder() async {
+    ui.Picture picture = await _loadPicture();
+    return await picture.toImage(_size.width.ceil(), _size.height.ceil());
+  }
+
+  Future<MemoryImage> _loadImageProviderFromPictureRecorder() async {
+    ui.Image image = await _loadImageFromPictureRecorder();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return MemoryImage(byteData!.buffer.asUint8List());
+  }
+
+  Future<ui.Image> _loadImageFromRepaintBoundary() async {
+    RenderRepaintBoundary boundary =
+        _repaintKey.currentContext?.findRenderObject() as RenderRepaintBoundary;
+    return await boundary.toImage();
+  }
+
+  Future<MemoryImage> _loadImageProviderFromRepaintBoundary() async {
+    ui.Image image = await _loadImageFromRepaintBoundary();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    return MemoryImage(byteData!.buffer.asUint8List());
+  }
+}
+
+class _Painter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.red
+      ..isAntiAlias = true;
+    canvas.drawLine(Offset.zero, Offset(size.width, size.height), paint);
+    canvas.drawLine(
+        Offset(size.width / 2, 0), Offset(size.width / 2, size.height), paint);
+    canvas.drawLine(
+        Offset(0, size.height / 2), Offset(size.width, size.height / 2), paint);
+    canvas.drawCircle(
+        Offset(3 * size.width / 4, size.height / 4), size.width / 10, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class _BufferPainter extends CustomPainter {
+  _BufferPainter({this.image, this.picture});
+
+  final ui.Image? image;
+  final ui.Picture? picture;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (image != null) {
+      canvas.drawImage(image!, Offset.zero, Paint());
+    } else if (picture != null) {
+      canvas.drawPicture(picture!);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
